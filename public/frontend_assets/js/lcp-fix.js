eddedaa6de1a8f4da5070529b0b0c43a2f1cc65d
HinaShahani <hania03hina@gmail.com>
2025-12-19 15:11:33 +0300

    LCP BreakDown

---
+++ public/frontend_assets/js/lcp-fix.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const lcpElement = document.querySelector('.slider-title');
+  if (lcpElement) {
+    lcpElement.style.visibility = 'visible';
+    lcpElement.style.opacity = '1';
+  }
+});
