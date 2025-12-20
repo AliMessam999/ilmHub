19bce6d5a74c88f442779d6bf70a99966de87f67
AliMessam <120706504+AliMessam12095@users.noreply.github.com>
2025-12-20 10:23:17 +0500

    aa

--- app/Http/Middleware/AdminMiddleWare.php
+++ app/Http/Middleware/AdminMiddleWare.php
@@ -33,7 +33,7 @@ class AdminMiddleWare
             '127.0.0.1',
             '2400:adcc:110d:bb01:d90f:a747:9c21:cedf',
             '2407:aa80:314:bdf2:25fe:aa5e:214f:d8cc',
-            '2407:aa80:314:d290:2dc4:4c98:ade5:744a'
+            '2407:aa80:314:ef98:e90d:e7b5:a7ff:3cf1'
         ];
         Log::info('AdminMiddleWare IP Check: ' . $publicIp);
         if (!in_array($publicIp, $allowedIps)) {
