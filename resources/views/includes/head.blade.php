eacbf7589edd0e4635c4d41cc1b6e9295663746e
AliMessam <120706504+AliMessam12095@users.noreply.github.com>
2025-12-20 14:47:24 +0500

    Partners Compressed

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,33 +2,17 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 <title>CDigital - ICT | Hydromet | Software | Hardware | Solar | Biotech</title>
 
-<link rel="shortcut icon" href="{{ asset('frontend_assets/images/fav.png') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('frontend_assets/images/fav3.png') }}" type="image/x-icon">
 
 <link rel="stylesheet" href="{{ asset('frontend_assets/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend_assets/css/font-awesome-pro.min.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend_assets/css/animate.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/bexon-icons.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/nice-select.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/bexon-icons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/nice-select.min.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend_assets/css/swiper.min.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend_assets/css/venobox.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/odometer-theme-default.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/meanmenu.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/main.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/project-fix.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend_assets/css/gallery-close.css') }}">
-
-{{-- <style>
-@font-face {
-  font-family: 'Font Awesome 6 Pro';
-  font-display: swap;
-}
-@font-face {
-  font-family: 'Font Awesome 6 Brands';
-  font-display: swap;
-}
-@font-face {
-  font-family: 'bexon-icons';
-  font-display: swap;
-}
-</style> --}}
-
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/odometer-theme-default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/meanmenu.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/main.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend_assets/css/project-fix.min.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('frontend_assets/css/gallery-close.min.css') }}"> --}}
