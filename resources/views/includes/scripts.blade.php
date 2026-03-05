5616ab7537f25b431af78a28bf89d593d20fa898
Adeel3330 <adeelansari3330@gmail.com>
2026-03-05 11:46:15 +0500

    new scroll

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -17,17 +17,3 @@
 <script defer src="{{ asset('frontend_assets/js/meanmenu.min.js') }}"></script>
 <script defer src="{{ asset('frontend_assets/js/main.js') }}"></script>
 
-<!-- Optimize reflow performance -->
-<script>
-    // Batch DOM reads and writes to minimize reflows
-    window.addEventListener('DOMContentLoaded', function() {
-        // Debounce scroll events to reduce reflow frequency
-        let scrollTimeout;
-        window.addEventListener('scroll', function() {
-            if (scrollTimeout) clearTimeout(scrollTimeout);
-            scrollTimeout = setTimeout(function() {
-                // Scroll handlers will run here with reduced frequency
-            }, 16); // ~60fps
-        }, { passive: true });
-    });
-</script>
